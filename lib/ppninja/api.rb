f9c2bbf70de8b0937a52ed08e8b2b352b0aa0bef
Yipeng Zhao <daggerjames.zhao@gmail.com>
2016-11-03 11:00:09 +0800

    抽出共有http_client对象

--- lib/ppninja/api.rb
+++ lib/ppninja/api.rb
@@ -1,58 +1,66 @@
-require 'json'
 require 'digest/md5'
-require 'rest-client'
+require 'fileutils'
 require_relative 'signature'
+require_relative 'http_client'
 
 module PPNinja
   class Api
-    attr_reader :appid, :appsecret, :host
+    attr_reader :client, :appid, :appsecret
     def initialize(appid, appsecret, host)
       @appid = appid
       @appsecret = appsecret
-      @host = host
+      @client = HttpClient.new(host)
     end
 
     def upload(file_path)
       # valid file and get file md5
       raise 'FilePathError' unless File.file?(file_path)
       md5 = Digest::MD5.hexdigest(File.read(file_path))
-      payload = {
-        file_source: File.new(file_path, 'rb'),
-      }
-      response = send_request({file_md5: md5}, payload, 'POST', '/api/job/create')
-      puts 'response: ', JSON.parse(response.body)
-      JSON.parse(response.body)["data"]["token"]
-    end
-
-    def status(token)
-      # token get from upload
-      response = send_request({token: token}, nil, 'GET', '/api/job/status')
-      puts 'response: ', JSON.parse(response.body)
-      JSON.parse(response.body)["data"]["state"]
+      res = post_file '/api/job/create', file_path, params: {file_md5: md5}
+      puts res
     end
 
     def download(token, file_path=:default)
       # token get from upload
       stat = status(token)
       return puts 'convert is not finishing.' unless stat == 'completed'
-
-      response = send_request({token: token}, nil, 'GET', '/api/job/download')
+      res = get '/api/job/download', params: {token: token}, content_type: 'application/zip'
+      # res is a Tempfile object
       file_path = "#{token}.zip" if file_path == :default
-      File.write(file_path, response.to_s)
+      FileUtils.cp res.path, file_path
+      puts "write file to #{file_path}"
+    end
+
+    def status(token)
+      res = get '/api/job/status', params: {token: token}
+      puts res
+      res["data"]["state"]
     end
 
     protected
+    def get(path, headers = {})
+      with_signature(path, 'GET', headers[:params]) do |authorization_token|
+        client.get path, headers.merge(:'x-ppj-authorization' => authorization_token)
+      end
+    end
 
-    def send_request(queries, payload, http_method, path)
-      timestamp = Time.now.to_i
-      signature = Signature.new(@appsecret).sign(queries, http_method, path, timestamp)
-      authorizationStr = 'Credential=' + @appid + ',Timestamp=' + timestamp.to_s + ',Signature=' + signature
+    def post(path, payload, headers = {})
+      with_signature(path, 'POST', headers[:params]) do |authorization_token|
+        client.post path, payload, headers.merge(:'x-ppj-authorization' => authorization_token)
+      end
+    end
 
-      RestClient::Request.execute(method: (http_method == 'POST' ? :post : :get), url: (@host + path).to_s,
-                                  timeout: 10, payload: payload, headers: {
-                                    params: queries,
-                                    'x-ppj-authorization' => authorizationStr
-                                  })
+    def post_file(path, file, headers = {})
+      with_signature(path, 'POST', headers[:params]) do |authorization_token|
+        client.post_file path, file, headers.merge(:'x-ppj-authorization' => authorization_token)
+      end
+    end
+
+    def with_signature(path, http_method, params)
+        timestamp = Time.now.to_i
+        signature = Signature.new(@appsecret).sign(params, http_method, path, timestamp)
+        authorizationStr = 'Credential=' + @appid + ',Timestamp=' + timestamp.to_s + ',Signature=' + signature
+        yield(authorizationStr)
     end
   end
 end
